4e3386dc43b1f1de92be7bf8b5efd5fed3821c7f
Marco Contreras <contacto@mcherrera.dev>
2025-07-29 16:08:41 -0400

    Add files via upload

---
+++ scripts-sql/rverde-ecommerce.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS rverde_ecommerce;
+USE rverde_ecommerce;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	correo VARCHAR(100) UNIQUE NOT NULL,
+	contrasena VARCHAR(255) NOT NULL,
+	direccion VARCHAR(255),
+	telefono VARCHAR(15),
+	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS categorias (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(150) NOT NULL,
+	descripcion TEXT
+);
+
+CREATE TABLE IF NOT EXISTS productos (
+	id BIGINT AUTO_INCREMENT NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	descripcion TEXT,
+	precio INT NOT NULL,
+	stock INT NOT NULL,
+	image_url VARCHAR(100),
+	categoria_id BIGINT,
+	FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE IF NOT EXISTS carrito (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	usuario_id BIGINT NOT NULL,
+	producto_id BIGINT NOT NULL,
+	cantidad INT NOT NULL,
+	fecha_agregado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+	FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
